816cef00f05bf89bdfb2f47e93ee618f0fa94966
Benjamin Wilson <12446446@students.lincoln.ac.uk>
2021-04-26 13:25:37 +0100

    Detention Centre Game
    An Upload of the game from backup

---
+++ Warden/Assets/Scripts/Interact.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/*
+To do:
+- Make a catcher for phrases, missions and aboding comments
+- Make a mission list
+- Make missions appear on UI
+*/
+public class Interact : MonoBehaviour
+{
+    public GameObject bubble,attention,dropdown,target;
+    public Text txt;
+    public TextAsset dialog;
+    private string uncompiled;
+    private List<string> eachLine;
+
+    private bool nextline;
+    public bool enabledropdown = false, partwaystop = false, enabletarget;
+    public int maxlines = 0;
+    public int place = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        nextline = false;
+        uncompiled = dialog.text;
+        eachLine = new List<string>();
+        eachLine.AddRange(uncompiled.Split("\n"[0]));
+        if(maxlines == 0){
+            maxlines = eachLine.Count;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyUp("return")){
+            nextline = false;
+        }
+    }
+
+    void PrintLine(string find, int loc){
+        
+
+        txt.text = eachLine[loc];
+        place +=1;
+        Debug.Log(txt.text);
+        /*float time = 1.0f;
+        for(int i = 0; i < eachLine.Count; i++)
+        {
+            string check = eachLine[i].Trim();
+            if(check.Equals(find)){
+
+            }
+            else{
+                char[] cycle = eachLine[i].ToCharArray();
+                int point = cycle.Length;
+                while(!nextline){
+                    txt.text = eachLine[i];
+                    if(nextline){
+                        while(time > 0.0f){
+                            time -= Time.deltaTime;
+                            Debug.Log(time);
+                        }
+                        txt.text += cycle[cycle.Length-point];
+                        point--;
+                        time = 1.0f;
+                    }
+                }
+
+            }
+        }*/
+    }
+
+    private void OnTriggerStay(Collider other) {
+        if(other.tag == "Player" && Input.GetKeyDown("return")){
+            Debug.Log("Enter");
+            if(nextline == false){
+                if(place >= maxlines){
+                    if(partwaystop){
+                        bubble.SetActive(false);
+                        attention.SetActive(false);
+                        maxlines = eachLine.Count;
+                        partwaystop = false;
+
+                        if(enabletarget){
+                            target.SetActive(true);
+                        }
+
+                        this.gameObject.SetActive(false);
+                    }
+                    else{
+                        if(enabledropdown == false)
+                        {
+                            bubble.SetActive(false);
+                            attention.SetActive(false);
+                        }
+                        else{
+                            
+                            attention.SetActive(false);
+                            dropdown.SetActive(true);
+                            if(dropdown.GetComponent<Dropdown>().value != 0){
+                                bubble.SetActive(false);
+                                dropdown.SetActive(false);
+                            }
+                        }
+                    }
+                }
+                else{
+                    bubble.SetActive(true);
+                    PrintLine("[1]", place);
+                }
+                nextline = true;
+            }
+        }
+    }
+}
---
+++ Warden/Assets/Scripts/OperateDoor.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OperateDoor : MonoBehaviour
+{
+    Vector3 closedpositionr;
+    Vector3 openpositionr;
+    Vector3 closedpositionl;
+    Vector3 openpositionl;
+    
+    float speed = 1.0f;
+    bool nearby = false;
+
+    public GameObject rightdoor;
+    public GameObject leftdoor;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        closedpositionl = leftdoor.transform.position;
+        closedpositionr = rightdoor.transform.position;
+        if(this.transform.rotation.y != 0.0f){
+            openpositionr = rightdoor.transform.position + new Vector3(0,0,-2);
+            openpositionl = leftdoor.transform.position + new Vector3(0,0,2);
+        }
+        
+        if(this.transform.rotation.y == 0.0f){
+            openpositionr = rightdoor.transform.position + new Vector3(2,0,0);
+            openpositionl = leftdoor.transform.position + new Vector3(-2,0,0);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch(nearby)
+        {
+            case true:
+                rightdoor.transform.position = openpositionr;
+                leftdoor.transform.position = openpositionl;
+                break;
+            case false:
+                rightdoor.transform.position = closedpositionr;
+                leftdoor.transform.position = closedpositionl;
+                break;
+            default:
+                break;
+        }   
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if(other.tag == "Player") nearby = true;
+    }
+
+    private void OnTriggerExit(Collider other) {
+        if(other.tag == "Player") nearby = false;
+    }
+}
